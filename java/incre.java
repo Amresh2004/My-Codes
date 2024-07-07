41c42687a9963cda78042a2aac5ff21260961417
Amresh <150937631+Amresh2004@users.noreply.github.com>
2024-07-07 20:48:49 +0530

    Add files via upload

---
+++ java/incre.java
@@ -0,0 +1,13 @@
+public class incre {
+    public static void main(String[] args) {
+        int a=10,b=20;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(++a);
+        System.out.println(a++);
+        System.out.println(a);
+        System.out.println(--b);
+        System.out.println(b--);
+        System.out.println(b);
+    }
+}
