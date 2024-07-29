446254701c7edf809d818b05515c2b9595c1847f
Amresh Warad <150937631+Amresh2004@users.noreply.github.com>
2024-07-29 22:22:07 +0530

    Update SetA4.py

--- FDS/Assignment 1/SetA4.py
+++ FDS/Assignment 1/SetA4.py
@@ -1,1 +1,23 @@
+import pandas as pd
 
+data = {
+    'name': ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'],
+    'age': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    'percentage': [85.5, 92.3, 78.6, 88.4, 79.5, 91.2, 85.7, 90.1, 87.6, 83.4]
+}
+
+df = pd.DataFrame(data)
+
+add_data = {
+    'name': ['K', 'L', None, 'N', 'O'],
+    'age': [23, 30, 25, None, 29],
+    'percentage': [85.5, None, 78.6, 79.5, 91.2]
+}
+
+add_df = pd.DataFrame(add_data)
+
+df = pd.concat([df, add_df], ignore_index=True)
+
+df['remarks'] = ""
+
+print(df)
