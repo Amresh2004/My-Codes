7ada09e7ce7645ca12a6fb9fa4f6cd7f473c66b1
Amresh Warad <150937631+Amresh2004@users.noreply.github.com>
2024-07-30 20:53:22 +0530

    Update Ass1C1Main.c

--- OS/Assignment 1/Ass1C1Main.c
+++ OS/Assignment 1/Ass1C1Main.c
@@ -4,68 +4,62 @@
 #include <sys/wait.h>
 #include <string.h>
 
-// Function to perform bubble sort
-void bubbleSort(int arr[], int n) {
+#define MAX_ARGS 128
+
+void sort(int arr[], int n) {
+    int temp;
     for (int i = 0; i < n-1; i++) {
-        for (int j = 0; j < n-i-1; j++) {
-            if (arr[j] > arr[j+1]) {
-                int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
+        for (int j = i+1; j < n; j++) {
+            if (arr[i] > arr[j]) {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
             }
         }
     }
 }
 
-int main() {
-    int n, i;
-    printf("Enter the number of elements: ");
-    scanf("%d", &n);
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("Usage: %s <list of integers>\n", argv[0]);
+        return 1;
+    }
 
+    int n = argc - 1;
     int arr[n];
-    printf("Enter %d integers:\n", n);
-    for (i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
+
+    for (int i = 0; i < n; i++) {
+        arr[i] = atoi(argv[i + 1]);
     }
 
-    // Sort the array
-    bubbleSort(arr, n);
+    sort(arr, n);
 
-    printf("Sorted array: ");
-    for (i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-
-    // Fork a child process
     pid_t pid = fork();
 
     if (pid < 0) {
-        fprintf(stderr, "Fork failed\n");
+        perror("fork failed");
         return 1;
     } else if (pid == 0) {
         // Child process
-        char *args[n + 3];
-        char buffer[20];
-        
-        args[0] = "./Ass1C1Second";
-        sprintf(buffer, "%d", n);
-        args[1] = buffer;
+        char *new_argv[MAX_ARGS];
+        new_argv[0] = "./binary_search"; // New program to be executed
 
-        for (i = 0; i < n; i++) {
-            sprintf(buffer, "%d", arr[i]);
-            args[i + 2] = strdup(buffer);
+        for (int i = 0; i < n; i++) {
+            new_argv[i + 1] = malloc(10);
+            sprintf(new_argv[i + 1], "%d", arr[i]);
         }
-        args[n + 2] = NULL;
 
-        execve("./Ass1C1Second", args, NULL);
-        perror("execve");
-        exit(1);
+        new_argv[n + 1] = NULL;
+
+        if (execve(new_argv[0], new_argv, NULL) == -1) {
+            perror("execve failed");
+        }
     } else {
         // Parent process
         wait(NULL);
-        printf("Child process completed\n");
     }
 
     return 0;
 }
+
+SAVE AS sort.c
