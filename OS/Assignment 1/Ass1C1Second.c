8d588606eb04bcab018003a01d2ff1f8729f45a8
Amresh Warad <150937631+Amresh2004@users.noreply.github.com>
2024-07-31 22:23:05 +0530

    Update Ass1C1Second.c

--- OS/Assignment 1/Ass1C1Second.c
+++ OS/Assignment 1/Ass1C1Second.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+/*#include <stdio.h>
 #include <stdlib.h>
 
 int binary_search(int arr[], int n, int target) {
@@ -62,3 +62,50 @@ OUTPUT:-
     Sorted array: 3 4 6 8
     Enter the number to search: 3
     Element found at index: 0
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int binary_search(int a[], int n, int target) {
+    int L = 0, R = n - 1;
+    while (L <= R) {
+        int mid = L + (R - L) / 2;
+        if (a[mid] == target) {
+            return mid;
+        }
+        if (a[mid] < target) {
+            L = mid + 1;
+        } else {
+            R = mid - 1;
+        }
+    }
+    return -1;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("Usage: %s <sorted integers>\n", argv[0]);
+        return 1;
+    }
+    
+    int n = argc - 1;
+    int arr[n];
+    for (int i = 0; i < n; ++i) {
+        arr[i] = atoi(argv[i + 1]);
+    }
+
+    int target;
+    printf("Enter the number to search: ");
+    scanf("%d", &target);
+
+    int result = binary_search(arr, n, target);
+    if (result != -1) {
+        printf("Element found at index %d\n", result);
+    } else {
+        printf("Element not found\n");
+    }
+    
+    return 0;
+}
+
