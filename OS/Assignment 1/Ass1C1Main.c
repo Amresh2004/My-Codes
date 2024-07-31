2edc4a98c2ecf7df03b21899211d03abc9294638
Amresh Warad <150937631+Amresh2004@users.noreply.github.com>
2024-07-31 11:02:08 +0530

    Update Ass1C1Main.c

--- OS/Assignment 1/Ass1C1Main.c
+++ OS/Assignment 1/Ass1C1Main.c
@@ -9,7 +9,7 @@
 void sort(int arr[], int n) {
     int temp;
     for (int i = 0; i < n-1; i++) {
-        for (int j = i+1; j < n; j++) {
+        for (int j = 0; j < n; j++) {
             if (arr[i] > arr[j]) {
                 temp = arr[i];
                 arr[i] = arr[j];
