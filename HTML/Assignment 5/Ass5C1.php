94fa0c71eb0b618287668fb5bcba599b13ee8688
Amresh Warad <150937631+Amresh2004@users.noreply.github.com>
2024-09-30 22:33:01 +0530

    Update Ass5C1.php

--- HTML/Assignment 5/Ass5C1.php
+++ HTML/Assignment 5/Ass5C1.php
@@ -1,28 +1,31 @@
 <?php
-$f=$_GET['t1'];
-$fp=fopen($f,"r");
-$ch=$_GET['r'];//option name r
-switch($ch)
-{
-case 1:
-echo "Type of file: ".filetype($f);
-break;
-case 2:
-echo "Last access time: ".date("F d Y H:i:s.",fileatime($f)); //day date year hr min seconds
-break;
-case 3:
-echo "File size: ".filesize($f);
-break;
-case 4:
-unlink($f);
-if(!unlink($f))
-{
-echo "<br>Error deleting file";
-}
-else
-{
-echo "File deleted";
-}
-break;
-}
+
+    $f = $_GET['t1'];
+    $ch = $_GET['r']; // option name r
+
+    if (file_exists($f)) {
+        switch ($ch) {
+            case 1:
+                echo "Type of file: " . filetype($f);
+                break;
+            case 2:
+                echo "Last access time: " . date("F d Y H:i:s.", fileatime($f)); // day date year hr min seconds
+                break;
+            case 3:
+                echo "File size: " . filesize($f) . " bytes";
+                break;
+            case 4:
+                if (unlink($f)) {
+                    echo "File deleted successfully.";
+                } else {
+                    echo "<br>Error deleting file";
+                }
+                break;
+            default:
+                echo "Invalid choice.";
+        }
+    }
+     else {
+        echo "File does not exist.";
+    }
 ?>
