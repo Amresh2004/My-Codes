25c6ab66ee3c76099d608bdd19c1449ff72aad93
Amresh Warad <150937631+Amresh2004@users.noreply.github.com>
2024-09-30 22:38:37 +0530

    Update Ass5B1.php

--- HTML/Assignment 5/Ass5B1.php
+++ HTML/Assignment 5/Ass5B1.php
@@ -1,22 +1,28 @@
-<!DOCTYPE html>
-<html>
-<body>
 <?php
-$filenm="item.dat";
-$fp=fopen($filenm,'r');
-?>
-<centre>
-<table border=2>
-<?php
-echo"<br><tr><th> item no</th><th> item name</th><th>unit sold</th><th>rate</th><th>total amt</th></tr></br>";
-while(!feof($fp))
+if(file_exists('item.dat'))
 {
-$x=explode(" ",fgets($fp));
-$t=$x[2]*$x[3];
-echo "<tr><td>$x[0]</td><td>$x[1]</td><td>$x[2]</td><td>$x[3]</td><td>$t</td></tr>";
+
+	$f=fopen('item.dat','r');
+	echo"<br><br>";
+	echo"<table border type=3 align=center>";
+	echo"<tr><td align=center>Item Code</td>";
+	echo"<td align=center>Item Name</td>";
+	echo"<td align=center>Unit Sold</td>";
+	echo"<td align=center>Rate</td>";
+	
+    while(!feof($f))
+	{
+	  $d=fgets($f);
+	  $s=explode(' ',$d);
+	 if(!empty($s[0]) && !empty($s[1]) && !empty($s[2]) && !empty($s[3]))
+{
+      echo"<tr><td align=center>$s[0]</td>"; 
+      echo"<td align=center>$s[1]</td>";
+      echo"<td align=center>$s[2]</td>";
+      echo"<td align=center>$s[3]</td>";
+
+}
+}
+echo"</table>";
 }
 ?>
-</table>
-</centre>
-</body>
-</html>
