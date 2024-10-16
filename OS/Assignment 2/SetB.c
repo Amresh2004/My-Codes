5886d5cc986a1ee9d33abc6ee6dff1ed041407e9
Amresh Warad <150937631+Amresh2004@users.noreply.github.com>
2024-10-16 14:43:22 +0530

    Update SetB.c

--- OS/Assignment 2/SetB.c
+++ OS/Assignment 2/SetB.c
@@ -7,75 +7,77 @@
 #include <dirent.h>
 #include <fcntl.h>
 
-void list(char c, char *dn) {
+void list(char c, char *dn) 
+{
     DIR *dir;
     int cnt = 0;
     struct dirent *entry;
     struct stat buff;
 
-    if ((dir = opendir(dn)) == NULL) {
+    if ((dir = opendir(dn)) == NULL) 
+    {
         printf("Directory %s not found\n", dn);
         return;
     }
 
-    switch (c) {
-    case 'f':
-        while ((entry = readdir(dir)) != NULL) {
-            printf("%s\n", entry->d_name);
-        }
-        break;
-    case 'n':
-        while ((entry = readdir(dir)) != NULL)
-            cnt++;
-        printf("Total No. of Entries = %d\n", cnt);
-        break;
-    case 'i':
-        while ((entry = readdir(dir)) != NULL) {
-            if (stat(entry->d_name, &buff) == 0) {
+    while ((entry = readdir(dir)) != NULL) 
+    {
+        switch (c) 
+        {
+            case 'f':
+                printf("%s\n", entry->d_name);
+                break;
+            case 'n':
+                cnt++;
+                break;
+            case 'i':
+                stat(entry->d_name, &buff);
                 printf("%s\t%ld\n", entry->d_name, (long)buff.st_ino);
-            } else {
-                perror("stat");
-            }
+                break;
+            default:
+                printf("Invalid option\n");
+                break;
         }
-        break;
-    default:
-        printf("Invalid option\n");
-        break;
     }
-
     closedir(dir);
 }
 
-int main() {
-    char command[80], t1[20], t2[20], t3[20], t4[20];
+int main() 
+{
+    char cmd[80], t1[20], t2[20], t3[20], t4[20];
     int n;
 
     system("clear");
 
-    while (1) {
+    while (1) 
+    {
         printf("myShell$ ");
         fflush(stdout);
 
-        if (fgets(command, 80, stdin) == NULL) {
+        if (fgets(cmd, 80, stdin) == NULL) 
+        {
             printf("\n");
             break;
         }
 
-        command[strcspn(command, "\n")] = 0;
+        cmd[strcspn(cmd, "\n")] = 0;
 
-        if (strcmp(command, "exit") == 0) {
+        if (strcmp(cmd, "exit") == 0) {
             printf("Exiting myShell.\n");
             break;
         }
 
-        n = sscanf(command, "%s %s %s %s", t1, t2, t3, t4);
+        n = sscanf(cmd, "%s %s %s %s", t1, t2, t3, t4);
 
-        if (strcmp(t1, "list") == 0) {
+        if (strcmp(t1, "list") == 0) 
+        {
             if (n >= 3)
                 list(t2[0], t3);
             else
                 printf("Usage: list <option> <directory>\n");
-        } else {
+        } 
+        else 
+        {
             printf("Unknown command, exiting.\n");
             exit(0);
         }
