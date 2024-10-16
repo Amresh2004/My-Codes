53e9bedd935c5ca25fe8602bb9e79e18b8a7fb55
Amresh Warad <150937631+Amresh2004@users.noreply.github.com>
2024-10-16 14:12:02 +0530

    Update SetAQ1.c

--- OS/Assignment 2/SetAQ1.c
+++ OS/Assignment 2/SetAQ1.c
@@ -7,29 +7,38 @@
 #include <dirent.h>
 #include <fcntl.h>
 
-void count(char c, char *fn) {
+void count(char c, char *fn) 
+{
     int lc = 0, wc = 0, cc = 0, handle;
     char ch;
 
-    if ((handle = open(fn, O_RDONLY)) == -1) {
+    if ((handle = open(fn, O_RDONLY)) == -1) 
+    {
         perror("Error opening file");
         return;
     }
 
-    while (read(handle, &ch, 1) > 0) {
-        if (ch == ' ') {
+    while (read(handle, &ch, 1) > 0) 
+    {
+        if (ch == ' ') 
+	{
             wc++;
-        } else if (ch == '\n') {
+        } 
+	else if (ch == '\n') 
+	{
             lc++;
             wc++;
-        } else {
+        } 
+	else 
+	{
             cc++;
         }
     }
 
     close(handle);
 
-    switch (c) {
+    switch (c) 
+    {
     case 'c':
         printf("Total No. of Characters = %d\n", cc);
         break;
@@ -45,66 +54,37 @@ void count(char c, char *fn) {
     }
 }
 
-int main() {
-    char command[80], t1[20], t2[20], t3[20], t4[20];
+int main() 
+{
+    char cmd[80], t1[20], t2[20], t3[20], t4[20];
     int n;
 
     system("clear");
 
-    while (1) {
+    while (1) 
+    {
         printf("myShell$ ");
         fflush(stdout);
 
-        fgets(command, 80, stdin);
-        command[strcspn(command, "\n")] = 0;
+        fgets(cmd, 80, stdin);
+        cmd[strcspn(cmd, "\n")] = 0;
 
-        if (strcmp(command, "exit") == 0) {
+        if (strcmp(cmd, "exit") == 0) 
+	{
             break;
         }
 
-        n = sscanf(command, "%s %s %s %s", t1, t2, t3, t4);
+        n = sscanf(cmd, "%s %s %s %s", t1, t2, t3, t4);
 
-        switch (n) {
-        case 1:
-            if (fork() == 0) {
-                execlp(t1, t1, NULL);
-                perror(t1);
-                exit(EXIT_FAILURE);
-            }
-            break;
-        case 2:
-            if (fork() == 0) {
-                execlp(t1, t1, t2, NULL);
-                perror(t1);
-                exit(EXIT_FAILURE);
-            }
-            break;
-        case 3:
-            if (strcmp(t1, "count") == 0) {
-                count(t2[0], t3);
-            } else {
-                if (fork() == 0) {
-                    execlp(t1, t1, t2, t3, NULL);
-                    perror(t1);
-                    exit(EXIT_FAILURE);
-                }
-            }
-            break;
-        case 4:
-            if (fork() == 0) {
-                execlp(t1, t1, t2, t3, t4, NULL);
-                perror(t1);
-                exit(EXIT_FAILURE);
-            }
-            break;
-        default:
-            printf("Invalid command or too many arguments\n");
-            break;
-        }
-
-        while (wait(NULL) > 0);
+        if (strcmp(t1, "count") == 0) 
+	{
+		count(t2[0], t3);
+	} 
+	else 
+	{
+		printf("Error"); 
+	}
     }
-
     printf("Exiting myShell.\n");
     return 0;
 }
