8050f588d2d720a892e8c35165088273ac27b75f
Amresh Warad <150937631+Amresh2004@users.noreply.github.com>
2024-10-18 00:07:12 +0530

    Update FIFO.c

--- OS/Assignment 4/FIFO.c
+++ OS/Assignment 4/FIFO.c
@@ -1,103 +1,34 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#define frame 3
 
-typedef struct queue 
-{
-	int data;
-	struct queue *next;
-} Node;
+int mem[frame] = {-1, -1, -1};
+int pagefault = 0;
+int n = 15;
+int a[] = {7, 3, 5, 8, 5, 8, 3, 6, 7, 3, 6, 7, 8, 5, 3};
+int front = 0;
 
-Node *start = NULL, *last = NULL, *ptr;
-int pgfault = 0, n, nof;
-int a[80];
-int n1, i;
-
-void printUI(int page, int page_fault) 
-{
-    printf("\n------------------------------------------------");
-    printf("\n Page Request : %2d ", page);
-    Node *temp;
-    for (temp = start; temp != NULL; temp = temp->next) 
-    {
-        printf(" |      %2d ", temp->data);
-    }
-    printf(" |   -->   Page Faults: %d", page_fault);
-    printf("\n------------------------------------------------");
-}
-
-int search(int page) 
-{
-    for (ptr = start; ptr != NULL; ptr = ptr->next) 
-    {
-        if (page == ptr->data)
-            return 1;
-    }
-    return 0;
-}
-
-void addlist(int page) 
-{
-    Node *ptr = (Node *)malloc(sizeof(Node));
-    ptr->data = page;
-    ptr->next = NULL;
-
-    if (start == NULL) 
-    {  
-        start = last = ptr;
-    } 
-    else if (nof < n) 
-    {  // Less than max frames
-        last->next = ptr;
-        last = ptr;
-    } 
-    else 
-    {  
-        Node *temp = start;
-        start = start->next; 
-        free(temp);
-        last->next = ptr;
-        last = ptr;
-    }
-
-    nof++;  
-}
-
-void print() 
-{
-    Node *ptr;
-    printf("\nCurrent Frame: ");
-    for (ptr = start; ptr != NULL; ptr = ptr->next)
-        printf("%d\t", ptr->data);
-    printf("\n\n");
-}
-
-int main() 
-{
-    printf("\nHow many Frames? ");
-    scanf("%d", &n);
-
-    printf("\nEnter the number of pages: ");
-    scanf("%d", &n1);
-
-    for (i = 0; i < n1; i++) 
-    {
-        printf("\nEnter the Page no %d: ", i + 1);
-        scanf("%d", &a[i]);
-    }
-
-    for (i = 0; i < n1; i++) 
-    {
-        if (search(a[i]) == 0) 
-	{ 
-            addlist(a[i]);  
-            pgfault++;
-            printf("\nPage fault Occurs");
-            print();
+void fifo(int p) {
+    int k;
+    for (k = 0; k < frame; k++) {
+        if (mem[k] == p) {
+            return;
         }
-        printUI(a[i], pgfault);
     }
-    
-    printf("\nTotal number of Page Faults = %d\n", pgfault);
-    
+    printf("\n\nPage fault");
+    pagefault++;
+    mem[front] = p;
+    front = (front + 1) % frame;
+}
+
+int main() {
+    int i, k;
+    for (i = 0; i < n; i++) {
+        fifo(a[i]);
+        for (k = 0; k < frame; k++) {
+            printf("%2d ", mem[k]);
+        }
+        printf("\n");
+    }
+    printf("\nTotal No of Pagefaults: %d", pagefault);
     return 0;
 }
