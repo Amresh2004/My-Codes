bd072ea888f6bb0f3404764c0a832e126c0bd26e
Amresh Warad <150937631+Amresh2004@users.noreply.github.com>
2024-10-17 23:26:38 +0530

    Update LRU.c

--- OS/Assignment 4/LRU.c
+++ OS/Assignment 4/LRU.c
@@ -1,46 +1,47 @@
-
-
-#include<stdio.h>
+#include <stdio.h>
 #define frame 3
-int mem[frame][2]={-1,0,-1,0,-1,0};
-int pagefault=0;
-int n=15;
-int a[]={7,3,5,8,5,8,3,6,7,3,6,7,8,5,3};
-void mfu(int p)
-{
-    int k,f=0;
-    for(k=0;k<frame;k++)
-    {
-        if(mem[k][0]==p)
-        { 
-           mem[k][1]=mem[k][1]+1;
-           return;
+
+int mem[frame] = {-1, -1, -1};
+int counter[frame] = {0, 0, 0};
+int pagefault = 0;
+int n = 15;
+int a[] = {7, 3, 5, 8, 5, 8, 3, 6, 7, 3, 6, 7, 8, 5, 3};
+
+void lru(int p, int time) {
+    int k, f = 0;
+    for (k = 0; k < frame; k++) {
+        if (mem[k] == p) {
+            counter[k] = time;
+            return;
         }
-    } 
-    printf("\n\n Page fault");
+    }
+    printf("\n\nPage fault");
     pagefault++;
-    for(k=0;k<frame;k++)
-       if(mem[k][0]==-1)
-       {
-          mem[k][0]=p;
-          mem[k][1]=0;
-          return;
-       }
-     for(k=0;k<frame;k++)
-        if(mem[k][1]<mem[f][1])
-            f=k;
-            mem[f][0]=p;
-            mem[f][1]=0;
+    for (k = 0; k < frame; k++) {
+        if (mem[k] == -1) {
+            mem[k] = p;
+            counter[k] = time;
+            return;
+        }
+    }
+    for (k = 1; k < frame; k++) {
+        if (counter[k] < counter[f]) {
+            f = k;
+        }
+    }
+    mem[f] = p;
+    counter[f] = time;
 }
-main()
-{
-    int i=0,k;
-    for(i=0;i<n;i++) 
-    {
-       mfu(a[i]);
-       for(k=0;k<frame;k++)
-           printf("%2d",mem[k][0]);
-           printf("\n\n");
-     }
-    printf("\nTotal No of Pagefault : %d",pagefault);
+
+int main() {
+    int i, k;
+    for (i = 0; i < n; i++) {
+        lru(a[i], i);
+        for (k = 0; k < frame; k++) {
+            printf("%2d ", mem[k]);
+        }
+        printf("\n");
+    }
+    printf("\nTotal No of Pagefaults: %d", pagefault);
+    return 0;
 }
