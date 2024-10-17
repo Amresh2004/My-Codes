96fb66c280b6974534d2588e2abcd4f67087ecd0
Amresh Warad <150937631+Amresh2004@users.noreply.github.com>
2024-10-18 00:09:01 +0530

    Update Optimal.c

--- OS/Assignment 4/Optimal.c
+++ OS/Assignment 4/Optimal.c
@@ -1,79 +1,58 @@
+#include <stdio.h>
+#define frame 3
 
-#include<stdio.h>
-#include<stdlib.h>
-typedef struct queue
-{
-     int data;
-     struct queue *next;
-}Node;
-Node *start=NULL,*last=NULL;
-int pgfault=0,n,nof;
-int a[80];
-int n1;
-main()
-{
-    int i;
-    printf("\nHow many Frames\t");
-    scanf("%d",&n);
-    printf("\n\nEnter the no of page\t");
-    scanf("%d",&n1);
-    for(i=0;i<n1;i++)
-    {
-         printf("\n\nEnter the Page no %d\t",i);
-         scanf("%d",&a[i]);
+int mem[frame] = {-1, -1, -1};
+int pagefault = 0;
+int n = 15;
+int a[] = {7, 3, 5, 8, 5, 8, 3, 6, 7, 3, 6, 7, 8, 5, 3};
+
+int predict(int index) {
+    int i, j, pos = -1, farthest = index;
+    for (i = 0; i < frame; i++) {
+        for (j = index; j < n; j++) {
+            if (mem[i] == a[j]) {
+                if (j > farthest) {
+                    farthest = j;
+                    pos = i;
+                }
+                break;
+            }
+        }
+        if (j == n) {
+            return i;
+        }
     }
-    for(i=0;i<n1;i++)
-    {
-	if(search(a[i])==0)
-	{
-		addlist(a[i]);
-		pgfault++;
-		printf("\nPage fault Occurs");
-		print();
-	}
+    return pos == -1 ? 0 : pos;
+}
+
+void opt(int p, int index) {
+    int k;
+    for (k = 0; k < frame; k++) {
+        if (mem[k] == p) {
+            return;
+        }
+    }
+    printf("\n\nPage fault");
+    pagefault++;
+    for (k = 0; k < frame; k++) {
+        if (mem[k] == -1) {
+            mem[k] = p;
+            return;
+        }
+    }
+    int pos = predict(index + 1);
+    mem[pos] = p;
+}
+
+int main() {
+    int i, k;
+    for (i = 0; i < n; i++) {
+        opt(a[i], i);
+        for (k = 0; k < frame; k++) {
+            printf("%2d ", mem[k]);
+        }
         printf("\n");
-        print();
-     }
-     printf("\n No of page Fault=%d",pgfault);
+    }
+    printf("\nTotal No of Pagefaults: %d", pagefault);
+    return 0;
 }
-int search(int page)
-{
-	Node *ptr;
-	for(ptr=start;ptr!=NULL;ptr=ptr->next)
-		if(ptr->data==page)
-			return 1;
-	return 0;
-}
-void print()
-{
-	Node *ptr;
-	for(ptr=start;ptr!=NULL;ptr=ptr->next)
-		printf("%d\t",ptr->data);
-	printf("\n");
-}
-void addlist(int page)
-{
-	Node *ptr=NULL;
-	static int cnt=0;
-	ptr=(Node *)malloc(sizeof(Node));
-	ptr->data=page;
-	ptr->next=NULL;
-	if(start==NULL)
-	{
-		start=last=ptr;
-		cnt++;
-	}
-	else if(cnt<n)
-	{
-		last->next=ptr;
-		last=ptr;
-		last->next=NULL;
-		cnt++;
-	}
-	else
-	{
-		last->data=ptr->data;
-	}
-}
-
-
