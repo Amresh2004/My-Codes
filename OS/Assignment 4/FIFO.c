45869a1b6eba188bc05a4ca740091d56b2b191ee
Amresh Warad <150937631+Amresh2004@users.noreply.github.com>
2024-10-18 11:34:43 +0530

    Update FIFO.c

--- OS/Assignment 4/FIFO.c
+++ OS/Assignment 4/FIFO.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
-#define N 3
+#define frame 3
 
-int mem[N] = {-1, -1, -1};
+int mem[frame] = {-1, -1, -1};
 int pagefault = 0;
 int n = 15;
-int a[] = {3,4,5,6,3,4,7,3,4,5,6,7,2,4,6};
+int a[] = {7, 3, 5, 8, 5, 8, 3, 6, 7, 3, 6, 7, 8, 5, 3};
 int front = 0;
 
 void fifo(int p) 
 {
     int i;
-    for (i = 0; i< N; i++) 
+    for (i = 0; i < frame; i++) 
     {
         if (mem[i] == p) 
         {
@@ -19,19 +19,16 @@ void fifo(int p)
     }
     printf("\n\nPage fault");
     pagefault++;
-    mem[N] = p;
-    N = (front + 1) % N;
+    mem[front] = p;
+    front = (front + 1) % frame;
 }
 
-int main() 
-{
-    int i, k;
-    for (i = 0; i < n; i++) 
-    {
+int main() {
+    int i, j;
+    for (i = 0; i < n; i++) {
         fifo(a[i]);
-        for (k = 0; k < N; k++) 
-        {
-            printf("%2d ", mem[k]);
+        for (j = 0; j < frame; j++) {
+            printf("%2d ", mem[j]);
         }
         printf("\n");
     }
